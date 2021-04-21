91d41133819bbb36b1efa9307567fceee2c18b73
Silse <thesilsemail@gmail.com>
2021-04-21 03:36:26 +0300

    Created DatabaseContext. Configured diffrent DbProviders.

---
+++ Compass.API/Contexts/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Compass.DataAccessLayer.Contexts
+{
+    public class DatabaseContext: DbContext
+    {
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
--- CompassSite/Startup.cs
+++ CompassSite/Startup.cs
@@ -1,5 +1,7 @@
+using Compass.DataAccessLayer.Contexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -15,6 +17,31 @@ namespace CompassSite
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            RegisterDatabaseContext(services);
+        }
+
+        private void RegisterDatabaseContext(IServiceCollection services)
+        {
+            string dbProvider = Configuration["DatabaseProvider"];
+            string connectionString = Configuration[$"ConnectionStrings:{dbProvider}"]; 
+            switch (dbProvider)
+            {
+                case "Postgres":
+                    {
+                        services.AddDbContext<DatabaseContext>(options => options.UseNpgsql(connectionString));
+                        break;
+                    }
+                case "MySql":
+                    {
+                        services.AddDbContext<DatabaseContext>(options => options.UseMySql(connectionString, ServerVersion.FromString(connectionString)));
+                        break;
+                    }
+                case "MsSql":
+                    {
+                        services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(connectionString));
+                        break;
+                    }
+            }
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
